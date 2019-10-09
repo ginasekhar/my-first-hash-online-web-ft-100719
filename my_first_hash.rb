3f7a7f279526f992ab7342866c8c784cf751f885
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-09 22:01:03 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,7 +18,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+ shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -30,8 +30,8 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
+  shipping_manifest["muskets"] = 2
+shipping_manifest["muskets"] = 4
 
   # add 4 gun powder to the shipping_manifest hash below
 
